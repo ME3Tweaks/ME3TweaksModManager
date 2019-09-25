a99df54f409909e36a7551e7e38c8c69ee7dfcd3
Mgamerz <mgamerz2@gmail.com>
2019-09-25 09:46:22 -0600

    Add testing project

---
+++ MassEffectModManagerCore.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using MassEffectModManager.modmanager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MassEffectModManagerCore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mod m = new Mod();
+        }
+    }
+}
