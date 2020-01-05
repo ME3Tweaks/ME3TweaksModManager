fe35dbc48ade8474b6254fb3afca0c42735d693a
Mgamerz <mgamerz2@gmail.com>
2020-01-05 12:48:46 -0700

    Change back to logupload instead of logupload2

--- MassEffectModManagerCore/modmanager/usercontrols/LogUploader.xaml.cs
+++ MassEffectModManagerCore/modmanager/usercontrols/LogUploader.xaml.cs
@@ -87,7 +87,7 @@ namespace MassEffectModManagerCore.modmanager.usercontrols
                     try
                     {
                         //this doesn't need to technically be async, but library doesn't have non-async method.
-                        string responseString = @"https://me3tweaks.com/modmanager/logservice/logupload2.php".PostUrlEncodedAsync(new { LogData = Convert.ToBase64String(lzmalog), ModManagerVersion = App.BuildNumber, CrashLog = isPreviousCrashLog }).ReceiveString().Result;
+                        string responseString = @"https://me3tweaks.com/modmanager/logservice/logupload.php".PostUrlEncodedAsync(new { LogData = Convert.ToBase64String(lzmalog), ModManagerVersion = App.BuildNumber, CrashLog = isPreviousCrashLog }).ReceiveString().Result;
                         Uri uriResult;
                         bool result = Uri.TryCreate(responseString, UriKind.Absolute, out uriResult)
                                       && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
