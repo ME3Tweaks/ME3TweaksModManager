069b802ed6433c76a6a5c0b1878b4c0ad60ad087
Mgamerz <mgamerz2@gmail.com>
2020-02-03 09:36:57 -0700

    Localizer: Add crash dialog

--- LocalizationHelper/App.xaml.cs
+++ LocalizationHelper/App.xaml.cs
@@ -4,8 +4,10 @@ using System.Configuration;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace LocalizationHelper
 {
@@ -14,5 +16,42 @@ namespace LocalizationHelper
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+                MessageBox.Show("App has crashed: " + FlattenException(ex));
+        }
+
+        private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("App has crashed: " + FlattenException(e.Exception));
+        }
+
+        /// <summary>
+        /// Flattens an exception into a printable string
+        /// </summary>
+        /// <param name="exception">Exception to flatten</param>
+        /// <returns>Printable string</returns>
+        public static string FlattenException(Exception exception)
+        {
+            var stringBuilder = new StringBuilder();
+
+            while (exception != null)
+            {
+                stringBuilder.AppendLine(exception.GetType().Name + ": " + exception.Message);
+                stringBuilder.AppendLine(exception.StackTrace);
+
+                exception = exception.InnerException;
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
