cef1d9b758c1c313b9fd7363f063b3ff9842d5d7
Mgamerz <mgamerz2@gmail.com>
2020-04-09 13:59:25 -0600

    Work on ME3CMM migration

---
+++ MassEffectModManagerCore/ui/BasicUITask.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using FontAwesome.WPF;
+
+namespace MassEffectModManagerCore.modmanager.windows
+{
+    /// <summary>
+    /// Class defining a basic UI task, that defaults to an gray hourglass, no spin, with no text.
+    /// </summary>
+    public class BasicUITask : INotifyPropertyChanged
+    {
+        public BasicUITask(string tasktext)
+        {
+            TaskText = tasktext;
+        }
+
+        public BasicUITask() { }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string TaskText { get; set; }
+        public bool Spin { get; set; } = false;
+        public Brush Foreground { get; set; } = Brushes.Gray;
+        public FontAwesomeIcon Icon { get; set; } = FontAwesomeIcon.Hourglass;
+    }
+}
