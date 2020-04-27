5c3fc24d59169c7a809bd0ddc025d7d26e6f5d00
Mgamerz <mgamerz2@gmail.com>
2020-04-27 07:21:21 -0600

    Let POL load with .xaml drop

--- MassEffectModManagerCore/App.xaml.cs
+++ MassEffectModManagerCore/App.xaml.cs
@@ -467,7 +467,7 @@ namespace MassEffectModManagerCore
             }
         }
 
-        public static string[] SupportedLanguages = { "int", /*"pol",*/ "rus", "deu", "fra"};
+        public static string[] SupportedLanguages = { "int", "pol", "rus", "deu", "fra"};
         public static Dictionary<string, string> ServerManifest { get; set; }
 
         public static int BuildNumber = Assembly.GetEntryAssembly().GetName().Version.Revision;
