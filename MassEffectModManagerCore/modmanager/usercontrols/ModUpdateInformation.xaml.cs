8ef355e33e7599373e2e1b90bde48b6c66547803
Mgamerz <mgamerz2@gmail.com>
2020-06-21 18:36:21 -0600

    Mod updater: Try to fix crash when progress is called after thread exits. Seems like it may need to be moved to the web client?

--- MassEffectModManagerCore/modmanager/usercontrols/ModUpdateInformation.xaml.cs
+++ MassEffectModManagerCore/modmanager/usercontrols/ModUpdateInformation.xaml.cs
@@ -245,7 +245,8 @@ namespace MassEffectModManagerCore.modmanager.usercontrols
                 ui.DownloadButtonText = M3L.GetString(M3L.string_downloading);
                 ui.ProgressChanged += (a, b) =>
                 {
-                    if (b.totalToDl != 0)
+                    if (b.totalToDl != 0 && nbw.IsBusy) //? IsBusy needs to be here for some reason
+
                     {
                         nbw.ReportProgress(0, b.currentDl * 1.0 / b.totalToDl);
                     }
