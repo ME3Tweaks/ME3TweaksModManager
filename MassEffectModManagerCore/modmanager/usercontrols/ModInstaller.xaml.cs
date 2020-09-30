ebdaa8493e96dc83289c41ae151f85806b9f2bee
Mgamerz <mgamerz2@gmail.com>
2020-09-30 15:02:34 -0600

    Catch missing failure case in mod install completed handler

--- MassEffectModManagerCore/modmanager/usercontrols/ModInstaller.xaml.cs
+++ MassEffectModManagerCore/modmanager/usercontrols/ModInstaller.xaml.cs
@@ -1046,14 +1046,20 @@ namespace MassEffectModManagerCore.modmanager.usercontrols
             var telemetryResult = ModInstallCompletedStatus.NO_RESULT_CODE;
             if (e.Error != null)
             {
-                Log.Error(@"An error occured during mod installation.");
+                Log.Error(@"An error occurred during mod installation.");
                 Log.Error(App.FlattenException(e.Error));
                 telemetryResult = ModInstallCompletedStatus.INSTALL_FAILED_EXCEPTION_IN_MOD_INSTALLER;
                 M3L.ShowDialog(mainwindow, M3L.GetString(M3L.string_interp_dialog_errorOccuredDuringInstallation, App.FlattenException(e.Error)), M3L.GetString(M3L.string_error));
             }
             else
             {
-                if (e.Result is ModInstallCompletedStatus mcis)
+                if (e.Result is (ModInstallCompletedStatus mcis1, string message1))
+                {
+                    Log.Error(@"An error occurred during mod installation.");
+                    telemetryResult = mcis1;
+                    M3L.ShowDialog(mainwindow, message1, mcis1.ToString()); // title won't be localized as it's error code
+                }
+                else if (e.Result is ModInstallCompletedStatus mcis)
                 {
                     telemetryResult = mcis;
                     //Success, canceled (generic and handled), ALOT canceled
