71ca7e68e318280c26935363549a4327958514be
Mgamerz <mgamerz2@gmail.com>
2020-11-17 11:58:22 -0700

    More retargeting

---
+++ MassEffectModManagerCore.Tests/TLKTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassEffectModManagerCore.Tests
+{
+    public class TLKTests
+    {
+        [TestMethod]
+        public void TestTLKs()
+        {
+
+        }
+    }
+}
