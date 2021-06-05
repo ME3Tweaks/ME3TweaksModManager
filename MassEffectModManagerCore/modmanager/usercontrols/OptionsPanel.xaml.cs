2eb110bb00c35b53e62ae010dffaa7491ec2c0a9
Mgamerz <mgamerz2@gmail.com>
2021-06-05 16:03:03 -0600

    basic options panel implementation

--- MassEffectModManagerCore/modmanager/usercontrols/OptionsPanel.xaml.cs
+++ MassEffectModManagerCore/modmanager/usercontrols/OptionsPanel.xaml.cs
@@ -13,30 +13,49 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MassEffectModManagerCore.ui;
+using PropertyChanged;
 
 namespace MassEffectModManagerCore.modmanager.usercontrols
 {
     /// <summary>
     /// Interaction logic for OptionsPanel.xaml
     /// </summary>
+    [AddINotifyPropertyChangedInterface]
     public partial class OptionsPanel : MMBusyPanelBase
     {
         public OptionsPanel()
         {
+            LibraryDir = Utilities.GetModsDirectory();
             LoadCommands();
             InitializeComponent();
         }
 
+
+        public string LibraryDir { get; set; }
         public ICommand CloseCommand { get; set; }
+        public ICommand ChangeLibraryDirCommand { get; set; }
 
         private void LoadCommands()
         {
             CloseCommand = new GenericCommand(() => OnClosing(DataEventArgs.Empty));
+            ChangeLibraryDirCommand = new GenericCommand(ChangeLibraryDir);
+        }
+
+        private void ChangeLibraryDir()
+        {
+
+            if (mainwindow.ChooseModLibraryPath(false))
+            {
+                LibraryDir = Settings.ModLibraryPath;
+            }
         }
 
         public override void HandleKeyPress(object sender, KeyEventArgs e)
         {
-
+            if (e.Key == Key.Escape)
+            {
+                OnClosing(DataEventArgs.Empty);
+            }
         }
 
         public override void OnPanelVisible()
