d7dfd065929057fa5536203ca31676df9985d2d6
Mgamerz <mgamerz2@gmail.com>
2021-06-16 14:02:07 -0600

    SQM Outfit Merge Stub

---
+++ MassEffectModManagerCore/modmanager/squadmates/SQMOutfitMerge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassEffectModManagerCore.modmanager.objects;
+
+namespace MassEffectModManagerCore.modmanager.squadmates
+{
+    class SQMOutfitMerge
+    {
+        public void BuildBioPGlobal(GameTarget target)
+        {
+
+        }
+    }
+}
