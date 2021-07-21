88982d099f49c8f95b90da9921d42b2fe5ea2c0a
Mgamerz <mgamerz2@gmail.com>
2021-07-21 09:13:00 -0600

    Add LEC scratchpad project for quick testing of things

---
+++ ScratchPadLEC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using LegendaryExplorerCore;
+using LegendaryExplorerCore.Packages;
+
+namespace ScratchPadLEC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LegendaryExplorerCoreLib.InitLib(TaskScheduler.Current);
+            MEPackageHandler.GlobalSharedCacheEnabled = false;
+
+            // Scratch projects here.
+            var package = MEPackageHandler.OpenMEPackage(@"C:\users\mgamerz\desktop\BioD_Nor_100Cabin.pcc");
+
+            var size1 = package.Imports[0].Header.Length;
+            foreach (var v in package.Imports)
+            {
+                if (v.Header.Length != size1)
+                    Debugger.Break();
+            }
+
+            var streamSaved = package.SaveToStream(true);
+            streamSaved.Position = 0;
+            var p2 = MEPackageHandler.OpenMEPackageFromStream(streamSaved);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
