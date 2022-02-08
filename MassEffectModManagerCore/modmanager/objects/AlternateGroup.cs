27414f8414759998ddcb32371400e1ac6cf33f50
Mgamerz <mgamerz2@gmail.com>
2022-02-07 19:27:30 -0700

    Sync commit: Begin work on new alternate options ui

--- MassEffectModManagerCore/modmanager/objects/AlternateGroup.cs
+++ MassEffectModManagerCore/modmanager/objects/AlternateGroup.cs
@@ -14,16 +14,45 @@ namespace ME3TweaksModManager.modmanager.objects
     [AddINotifyPropertyChangedInterface]
     public class AlternateGroup
     {
-        public string GroupName { get; }
+        /// <summary>
+        /// All alternate option choices
+        /// </summary>
         public ObservableCollectionExtended<AlternateOption> AlternateOptions { get; } = new();
+
+        /// <summary>
+        /// All alternate option choices that are not the selected option, for use in the drop down
+        /// </summary>
+        public ObservableCollectionExtended<AlternateOption> OtherOptions { get; } = new();
+
+
+        /// <summary>
+        /// The currently selected option. If there is only one option, this is always that option.
+        /// </summary>
         public AlternateOption SelectedOption { get; set; }
+
+        /// <summary>
+        /// The displayed group name
+        /// </summary>
+        public string GroupName { get; init; }
 
         public AlternateGroup(List<AlternateOption> options)
         {
             // Find the already selected one
+            if (options == null || options.Count == 0)
+                throw new Exception(@"AlternateGroup being generated with null or empty list of options!");
+            GroupName = options[0].GroupName;
             AlternateOptions.Add(options.First(x => x.CheckedByDefault));
             AlternateOptions.AddRange(options.Where(x => !x.CheckedByDefault));
             SelectedOption = AlternateOptions.FirstOrDefault();
+        }
+
+        public void OnSelectedOptionChanged()
+        {
+            var optionsList = AlternateOptions.Where(x => x != SelectedOption).ToList();
+            // Todo: Sort non-selectable to the bottom
+
+
+            OtherOptions.ReplaceAll(optionsList);
         }
     }
 }
---
+++ MassEffectModManagerCore/modmanager/usercontrols/modinstaller/AlternateOptionSelector.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ME3TweaksModManager.modmanager.objects;
+using PropertyChanged;
+
+namespace ME3TweaksModManager.modmanager.usercontrols.modinstaller
+{
+    /// <summary>
+    /// Control that allows for selection of an alternate. Non-grouped alternates are displayed using a simple checkbox; grouped alternates will display a dropdown
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public partial class AlternateOptionSelector : UserControl
+    {
+        /// <summary>
+        /// The group containing the options that this selector can present. If the group has only one item, it is converted to a checkbox. The group will be the datacontext
+        /// </summary>
+        //private AlternateGroup Group => DataContext as AlternateGroup;
+
+        public AlternateOptionSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void AlternateOptionSelector_OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
