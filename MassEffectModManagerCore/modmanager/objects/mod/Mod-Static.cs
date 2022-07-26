88f561bf77868ef10bc83332fdf817f4fdb5b0ca
Mgamerz <mgamerz2@gmail.com>
2022-07-25 21:22:50 -0600

    Add static methods to Mod class for getting mod version from ini

---
+++ MassEffectModManagerCore/modmanager/objects/mod/Mod-Static.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LegendaryExplorerCore.Misc;
+using ME3TweaksCore.Misc;
+
+namespace ME3TweaksModManager.modmanager.objects.mod
+{
+    // Static methods for Mod class
+    public partial class Mod
+    {
+        /// <summary>
+        /// Reads the 'modver' descriptor from a mod manager moddesc file. Used to query a version on disk without loading the mod.
+        /// </summary>
+        /// <param name="modDescPath">Path to the moddesc</param>
+        /// <returns>Version (proper), null if failed to parse</returns>
+        public static Version GetModVersionFromIni(string modDescPath)
+        {
+            DuplicatingIni ini = DuplicatingIni.LoadIni(modDescPath);
+            var version = ini[@"ModInfo"][@"modver"];
+            if (version != null && version.HasValue)
+            {
+                if (ProperVersion.TryParse(version.Value, out var result))
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+    }
+}
