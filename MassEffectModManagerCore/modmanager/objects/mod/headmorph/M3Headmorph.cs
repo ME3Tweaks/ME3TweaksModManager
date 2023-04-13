3bdb50e60ce085d68ae8dd0c7398f1db8f31747a
Mgamerz <mgamerz@msn.com>
2023-04-12 23:36:09 -0600

    M3HM - Implement IM3ImageEnabled interface

--- MassEffectModManagerCore/modmanager/objects/mod/headmorph/M3Headmorph.cs
+++ MassEffectModManagerCore/modmanager/objects/mod/headmorph/M3Headmorph.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using LegendaryExplorerCore.Misc;
 using ME3TweaksCore.Helpers;
 using ME3TweaksModManager.modmanager.objects.mod.editor;
 using ME3TweaksModManager.modmanager.objects.mod.interfaces;
+using ME3TweaksModManager.modmanager.usercontrols.interfaces;
 using Newtonsoft.Json;
 
 namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
@@ -14,12 +16,14 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
     /// <summary>
     /// A ModDesc-mod headmorph reference - pinned to a .me3headmorph and .ron file
     /// </summary>
-    public class M3Headmorph : M3ValidateOnLoadObject
+    public class M3Headmorph : M3ValidateOnLoadObject, IM3ImageEnabled
     {
         /// <summary>
         /// Blank constructor
         /// </summary>
-        public M3Headmorph() { }
+        public M3Headmorph()
+        {
+        }
 
         /// <summary>
         /// The filename of the headmorph
@@ -39,13 +43,22 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
         /// <summary>
         /// The image asset to show in the the user interface when this headmorph is to be presented to the user, as part of a preview
         /// </summary>
-        public string ImageAsset { get; set; }
+        public string ImageAssetName { get; set; }
+
+        /// <summary>
+        /// The height of the image to display when shown in a tooltip
+        /// </summary>
+        [JsonIgnore]
+        public int ImageHeight { get; set; }
 
         #region PARAMETERS
+
         private const string FILENAME_PARM = @"Filename";
         private const string TITLE_PARM = @"Title";
         private const string DESCRIPTION_PARM = @"Description";
         private const string IMAGE_PARM = @"ImageAsset";
+        private const string IMAGE_HEIGHT_PARM = @"ImageHeight";
+
         #endregion
 
         /// <summary>
@@ -59,7 +72,8 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
             if (!ValidateParameters(nameof(M3Headmorph), parms, FILENAME_PARM, TITLE_PARM))
                 return;
 
-            if (ValidateFileParameter(mod, nameof(M3Headmorph), parms, FILENAME_PARM, Mod.HEADMORPHS_FOLDER_NAME, required: true))
+            if (ValidateFileParameter(mod, nameof(M3Headmorph), parms, FILENAME_PARM, Mod.HEADMORPHS_FOLDER_NAME,
+                    required: true))
             {
                 FileName = parms[FILENAME_PARM];
             }
@@ -72,10 +86,26 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
             Title = parms[TITLE_PARM];
 
             Description = parms.ContainsKey(DESCRIPTION_PARM) ? parms[DESCRIPTION_PARM] : null;
-            if (parms.ContainsKey(IMAGE_PARM) && ValidateImageParameter(mod, nameof(M3Headmorph), parms, IMAGE_PARM, false, false)) // Should headmorphs be installable directly from archive...?
+            if (parms.ContainsKey(IMAGE_PARM) && ValidateImageParameter(mod, nameof(M3Headmorph), parms, IMAGE_PARM, false,
+                    false,
+                    additionalRequiredParam:
+                    IMAGE_HEIGHT_PARM)) // Should headmorphs be installable directly from archive...?
             {
-                ImageAsset = parms[IMAGE_PARM];
+                ImageAssetName = parms[IMAGE_PARM];
+                if (int.TryParse(parms[IMAGE_HEIGHT_PARM], out var imageHeight))
+                {
+                    if (imageHeight < 1)
+                    {
+                        M3Log.Error($@"{IMAGE_HEIGHT_PARM} value must be an integer greater than 0.");
+                        ValidationFailedReason = $"{IMAGE_HEIGHT_PARM} value must be an integer greater than 0.";
+                        return;
+                    }
+
+                    ImageHeight = imageHeight;
+                }
             }
+
+            ModdescMod = mod;
         }
 
         /// <summary>
@@ -86,13 +116,19 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
         public IEnumerable<string> GetRelativeReferences(Mod mod)
         {
             List<string> references = new List<string>();
-            var fullPath = FilesystemInterposer.PathCombine(mod.IsInArchive, mod.ModPath, Mod.HEADMORPHS_FOLDER_NAME, FileName);
-            references.Add((mod.IsInArchive && mod.ModPath.Length == 0) ? fullPath : fullPath.Substring(mod.ModPath.Length + 1)); // Add the headmorph file
+            var fullPath =
+                FilesystemInterposer.PathCombine(mod.IsInArchive, mod.ModPath, Mod.HEADMORPHS_FOLDER_NAME, FileName);
+            references.Add((mod.IsInArchive && mod.ModPath.Length == 0)
+                ? fullPath
+                : fullPath.Substring(mod.ModPath.Length + 1)); // Add the headmorph file
 
-            if (ImageAsset != null)
+            if (ImageAssetName != null)
             {
-                var imageAssetPath = FilesystemInterposer.PathCombine(mod.IsInArchive, mod.ModImageAssetsPath, ImageAsset);
-                references.Add((mod.IsInArchive && mod.ModPath.Length == 0) ? imageAssetPath : imageAssetPath.Substring(mod.ModPath.Length + 1)); // Add the headmorph file
+                var imageAssetPath =
+                    FilesystemInterposer.PathCombine(mod.IsInArchive, mod.ModImageAssetsPath, ImageAssetName);
+                references.Add((mod.IsInArchive && mod.ModPath.Length == 0)
+                    ? imageAssetPath
+                    : imageAssetPath.Substring(mod.ModPath.Length + 1)); // Add the headmorph file
             }
 
             return references;
@@ -102,7 +138,8 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
         /// Parameter map, used for the moddesc.ini editor Contains a list of values in the alternate mapped to their string value
         /// </summary>
         [JsonIgnore]
-        public ObservableCollectionExtended<MDParameter> ParameterMap { get; } = new ObservableCollectionExtended<MDParameter>();
+        public ObservableCollectionExtended<MDParameter> ParameterMap { get; } =
+            new ObservableCollectionExtended<MDParameter>();
 
         /// <summary>
         /// List of all keys in the M3MEMMod struct that are publicly available for editing
@@ -113,13 +150,22 @@ namespace ME3TweaksModManager.modmanager.objects.mod.headmorph
             var parameterDictionary = new Dictionary<string, object>()
             {
                 // List of available parameters for this object
-                {FILENAME_PARM, FileName},
-                {TITLE_PARM, Title},
-                {DESCRIPTION_PARM, Description},
-                {IMAGE_PARM, new MDParameter(@"string", IMAGE_PARM, ImageAsset, new [] {@""}, "") { AllowedValuesPopulationFunc = mod.PopulateImageOptions}}, // Uses image population function
+                { FILENAME_PARM, FileName },
+                { TITLE_PARM, Title },
+                { DESCRIPTION_PARM, Description },
+                {
+                    IMAGE_PARM,
+                    new MDParameter(@"string", IMAGE_PARM, ImageAssetName, new[] { @"" }, "")
+                        { AllowedValuesPopulationFunc = mod.PopulateImageOptions }
+                }, // Uses image population function
+                { IMAGE_HEIGHT_PARM, ImageHeight },
             };
 
             ParameterMap.ReplaceAll(MDParameter.MapIntoParameterMap(parameterDictionary));
         }
+
+        // IM3ImageEnabled interface
+        public Mod ModdescMod { get; set; }
+        public BitmapSource ImageBitmap { get; set; }
     }
 }
