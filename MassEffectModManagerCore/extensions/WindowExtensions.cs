43981545c02483691f2113002d791afb72c72da5
Mgamerz <mgamerz@msn.com>
2023-08-19 14:08:26 -0600

    Change title bar theming

--- MassEffectModManagerCore/extensions/WindowExtensions.cs
+++ MassEffectModManagerCore/extensions/WindowExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Dark.Net;
+﻿using System.Diagnostics;
+using System.Drawing;
+using Dark.Net;
 using ME3TweaksModManager.modmanager;
 using System.Windows;
 
@@ -6,6 +8,16 @@ namespace ME3TweaksModManager.extensions
 {
     public static class WindowExtensions
     {
+        /// <summary>
+        /// Converts a media color to a drawing color
+        /// </summary>
+        /// <param name="mediaColor"></param>
+        /// <returns></returns>
+        private static Color ToDrawingColor(this System.Windows.Media.Color mediaColor)
+        {
+            return Color.FromArgb(mediaColor.A, mediaColor.R, mediaColor.G, mediaColor.B);
+        }
+
         public static void ApplyDarkNetWindowTheme(this Window window)
         {
             if (Settings.SkipDarkNet)
@@ -13,7 +25,17 @@ namespace ME3TweaksModManager.extensions
                 return;
             }
 
-            DarkNet.Instance.SetWindowThemeWpf(window, Settings.DarkTheme ? Theme.Dark : Theme.Light);
+            var titleColor = (System.Windows.Media.Color)window.FindResource(AdonisUI.Colors.Layer0BackgroundColor);
+            var textColor = (System.Windows.Media.Color)window.FindResource(AdonisUI.Colors.ForegroundColor);
+            var borderColor = (System.Windows.Media.Color)window.FindResource(AdonisUI.Colors.Layer1BorderColor);
+
+
+            DarkNet.Instance.SetWindowThemeWpf(window, Settings.DarkTheme ? Theme.Dark : Theme.Light, new ThemeOptions()
+            {
+                TitleBarBackgroundColor = titleColor.ToDrawingColor(),
+                TitleBarTextColor = textColor.ToDrawingColor(),
+                WindowBorderColor = borderColor.ToDrawingColor(),
+            });
         }
     }
 }
