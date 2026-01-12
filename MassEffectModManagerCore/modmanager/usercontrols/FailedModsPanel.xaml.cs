17d7bc725cc017f5f81ab2e555e3adac5b2fe4d5
Mgamerz <mgamerz@msn.com>
2026-01-12 07:58:46 -0700

    Localize FailedModsPanel

--- MassEffectModManagerCore/modmanager/usercontrols/FailedModsPanel.xaml.cs
+++ MassEffectModManagerCore/modmanager/usercontrols/FailedModsPanel.xaml.cs
@@ -131,7 +131,7 @@ namespace ME3TweaksModManager.modmanager.usercontrols
                 if (result != null)
                 {
                     // Issue opening the file.
-                    M3L.ShowDialog(window, $"Error opening moddesc.ini file: {result}", M3L.GetString(M3L.string_error), MessageBoxButton.OK, MessageBoxImage.Error);
+                    M3L.ShowDialog(window, M3L.GetString(M3L.string_interp_errorOpeningModdesciniFileResult, result), M3L.GetString(M3L.string_error), MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
